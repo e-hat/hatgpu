362145d11754b2ba0bced7746cdeeb72ac29ad13
Edward Hatfield <edward.hatfield@tufts.edu>
2024-12-13 03:36:40 -0500

    Pass scene name via absl::Flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include "application/Application.h"
 #include "hatpch.h"
 
+#include <absl/flags/flag.h>
+#include <absl/flags/parse.h>
+
 #include <memory>
 
-int main()
+ABSL_FLAG(std::string, scene_path, "../scenes/sponza.json", "The path to the scene json file.");
+
+int main(int argc, char **argv)
 {
-    auto app = std::make_unique<hatgpu::Application>("HatGPU", "../scenes/sponza.json");
+    absl::ParseCommandLine(argc, argv);
+    auto app = std::make_unique<hatgpu::Application>("HatGPU", absl::GetFlag(FLAGS_scene_path));
     app->Init();
     app->Run();
 
